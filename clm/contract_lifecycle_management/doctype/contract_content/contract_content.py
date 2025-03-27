caed9222904a87c3f77dd0085e349fd9df2ba8e0
ManyaGirdhar <girdharmanya28@gmail.com>
2025-03-27 10:32:00 +0530

    feat:created Contrcat webform

---
+++ clm/contract_lifecycle_management/doctype/contract_content/contract_content.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Manya and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+
+class ContractContent(WebsiteGenerator):
+	pass
