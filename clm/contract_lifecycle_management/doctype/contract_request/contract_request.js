f38533da712783162db57da2a7450ed3547735b6
ManyaGirdhar <girdharmanya28@gmail.com>
2025-04-11 12:41:19 +0530

    chore: added a create contract button

--- clm/contract_lifecycle_management/doctype/contract_request/contract_request.js
+++ clm/contract_lifecycle_management/doctype/contract_request/contract_request.js
@@ -1,8 +1,41 @@
 // Copyright (c) 2025, Manya and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Contract Request", {
-// 	refresh(frm) {
 
-// 	},
-// });
+
+frappe.ui.form.on('Contract Request', {
+    refresh(frm) {
+        // ✅ Replace Submit button label with "Approve"
+        if (!frm.doc.__islocal && frm.doc.docstatus === 0) {
+            frm.page.set_primary_action(__('Approve'), () => {
+                frappe.confirm(
+                    'Are you sure you want to approve this Contract Request?',
+                    () => {
+                        frm.save('Submit');  // Triggers standard submit behavior
+                    },
+                    () => {
+                        frappe.msgprint('Approval cancelled.');
+                    }
+                );
+            });
+        }
+
+        // ✅ Show "Create Contract" only when request is submitted
+        if (frm.doc.docstatus === 1) {
+        // ✅ Add "Create Contract" button
+            frm.add_custom_button(__('Create Contract'), function () {
+                frappe.model.open_mapped_doc({
+                    method: "clm.contract_lifecycle_management.doctype.contract_request.contract_request.create_contract_from_request",
+                    frm: frm
+                });
+            });
+        }
+    }
+});
+
+
+
+
+
+
+
--- clm/contract_lifecycle_management/doctype/contract_request/contract_request.py
+++ clm/contract_lifecycle_management/doctype/contract_request/contract_request.py
@@ -1,9 +1,29 @@
 # Copyright (c) 2025, Manya and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
 
 class ContractRequest(Document):
 	pass
+        
+@frappe.whitelist()
+def create_contract_from_request(source_name):
+	# Get the ContractRequest document
+	contract_request = frappe.get_doc("Contract Request", source_name)
+
+	# Create a new Contract document
+	contract = frappe.new_doc("Contract")
+
+	# Map fields from ContractRequest to Contract
+	contract.contract_type = contract_request.contract_type
+	contract.title = contract_request.title
+	contract.contract_duration = contract_request.contract_duration
+	contract.requester_type = contract_request.requester_type
+	contract.counterparty_name = contract_request.requester_name
+	contract.counterparty_email = contract_request.requester_email
+	contract.description = contract_request.description
+	contract.request_id = contract_request.name  # Optional: Add reference back to request
+
+	# Return new document so open_mapped_doc can handle it
+	return contract
