45c0f3b42199a16f3b835bee8238324d0f35ec10
ManyaGirdhar <girdharmanya28@gmail.com>
2026-06-27 16:49:26 +0530

    feat: add Gemini and Documenso Settings doctype configurations

---
+++ clm/contract_lifecycle_management/doctype/documenso_settings/documenso_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Manya and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class DocumensoSettings(Document):
+	pass
